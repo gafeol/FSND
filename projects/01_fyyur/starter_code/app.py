83a39b014fb665055e56666ec0c45043435b09fb
gafeol <gabfeol@gmail.com>
2020-08-04 15:25:56 -0300

    Remove prints, limpa codigo, adiciona edicao de artista

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -106,7 +106,6 @@ def get_num_upcoming_shows(venue):
 def index():
   return render_template('pages/home.html')
 
-
 #  Venues
 #  ----------------------------------------------------------------
 
@@ -137,7 +136,7 @@ def venues():
     }]
   }]
   '''
-  venues = Venue.query.all()
+  venues = Venue.query.order_by(Venue.id).all()
   auxDict = {}
   for v in venues:
     auxDict[(v.city, v.state)] = auxDict.get((v.city, v.state), []) + [v]
@@ -274,30 +273,18 @@ def create_venue_submission():
   error = False
   try:
     form = VenueForm(request.form)
-    print(form.name.data)
     venue = Venue(
       name=form.name.data,
       city=form.city.data,
       state=form.state.data,
       phone=form.phone.data,
-     # genres="['" + form.genres.data + "']",
+      #genres=form.genres.data,
       #image_link=form.image_link.data,
       #facebook_link=form.facebook_link.data,
       address=form.address.data
     )
-    print("PRINTANDO VENUE+++++++++++++++++")
-    print(venue.name)
-    print(venue.city)
-    print(venue.state)
-    print(venue.phone)
-    #print(venue.genres)
-    #print(venue.image_link)
-    #print(venue.facebook_link)
-    print(venue.address)
     db.session.add(venue)
-    print("PAssou do ADDD")
     db.session.commit()
-    print("COMMITOU")
   except:
     error = True
     db.session.rollback()
@@ -349,7 +336,7 @@ def artists():
     "name": "The Wild Sax Band",
   }]
   '''
-  data = Artist.query.with_entities(Artist.id, Artist.name).all()
+  data = Artist.query.with_entities(Artist.id, Artist.name).order_by(Artist.id).all()
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -368,7 +355,7 @@ def search_artists():
   matching = Artist.query.filter(Artist.name.ilike('%'+search_term+'%')).all()
   response = {
     "count": len(matching),
-    "data": matching # TODO: Arrumar o num upcoming shows
+    "data": matching 
   }
 
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
@@ -459,6 +446,7 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
+  '''
   artist={
     "id": 4,
     "name": "Guns N Petals",
@@ -472,19 +460,34 @@ def edit_artist(artist_id):
     "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
     "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
   }
-  # TODO: populate form with fields from artist with ID <artist_id>
+  '''
+  artist = Artist.query.filter_by(id=artist_id).first()
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
+  try:
+    artist = Artist.query.get(artist_id)
+    artist.name = request.form['name']
+    artist.city = request.form['city']
+    artist.state = request.form['state']
+    artist.phone = request.form['phone']
+    db.session.commit()
+    flash('Artist ' + artist.name + ' edited successfully!')
+  except:
+    flash('Error while editing artist!')
+    db.session.rollback()
+  finally:
+    db.session.close()
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
+  '''
   venue={
     "id": 1,
     "name": "The Musical Hop",
@@ -499,7 +502,8 @@ def edit_venue(venue_id):
     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
     "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   }
-  # TODO: populate form with values from venue with ID <venue_id>
+  '''
+  venue = Venue.query.get(venue_id)
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
