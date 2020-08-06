53cbadb6938c0a48a9031ece6644d29f87220220
gafeol <gabfeol@gmail.com>
2020-08-06 14:58:28 -0300

    Separates on models file, solves requirements

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -6,79 +6,18 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from models import db, app, moment, Venue, Show, Artist
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    genres = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='venue')
-
-    def __repr__(self):
-      return f'<Venue {self.id} {self.name}>'
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    website = db.Column(db.String(120))
-    shows = db.relationship('Show', backref='artist')
-
-    def __repr__(self):
-      return f'<Artist {self.id} {self.name}>'
-
-class Show(db.Model):
-  __tablename__ = 'Show'
-
-  id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-  start_time = db.Column(db.DateTime)
-
-  def __repr__(self):
-    return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
-
-#----------------------------------------------------------------------------#
-# Filters.
-#----------------------------------------------------------------------------#
-
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
   if format == 'full':
@@ -88,13 +27,6 @@ def format_datetime(value, format='medium'):
   return babel.dates.format_datetime(date, format)
 
 app.jinja_env.filters['datetime'] = format_datetime
-
-def get_num_upcoming_shows(venue):
-  count = 0
-  for show in venue.shows:
-    if show.start_time > datetime.today():
-      count = count + 1
-  return count
 
 #----------------------------------------------------------------------------#
 # Controllers.
---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,57 @@
+
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+
+app = Flask('app')
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    genres = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue')
+
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    website = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='artist')
+
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
+
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+  start_time = db.Column(db.String)
+
+  def __repr__(self):
+    return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
